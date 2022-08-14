acc43e62c0010718d7437d6dc02dcb8075284665
viktorberezovskyi <viktor.berezovskij@mail.zenbit.tech>
2022-08-14 12:31:52 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {ProductService} from "./services/product.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-crush';
+export class AppComponent implements OnInit {
+  title = 'angular app';
+  term = '';
+  products: IProduct[] = [];
+  loading = false
+  products$: Observable<IProduct[]>
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productService.getAll().subscribe((products) => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
 }
